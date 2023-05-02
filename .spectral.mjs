7fcef36077647ade8ef902f4048f0a492b435b81
qa-excoy-2-grtj@example.com <qa-excoy-2-grtj@example.com>
2023-05-02 17:16:51 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjE4ODA4Mw/spectral.js?branch_slug=main&token=9f2fafe1-6f63-45b7-a52f-1e0d8a798dab";
+export default { extends: ruleset };
